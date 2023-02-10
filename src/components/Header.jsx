0df4689f0a475aa6bc38e812c3b353c6d6239a16
WilintonAscanio <wilintonascanio12@hotmail.com>
2023-02-10 15:11:35 -0500

    Add structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <section>
+                    <small>loopstudios</small>
+                    <nav>
+                        <ul>
+                            <li>About</li>
+                            <li>Careers</li>
+                            <li>Events</li>
+                            <li>Products</li>
+                            <li>Support</li>
+                        </ul>
+                    </nav>
+                </section>
+                <div>
+                    <h1>INMERSIVE <br />
+                        EXPERIENCES <br />
+                        THAT DELIVER</h1>
+                </div>
+
+            </header>
+        </>
+    )
+}
+
+export default Header

d3c53967e77c203d2379187faf396ff50a81a23c
WilintonAscanio <wilintonascanio12@hotmail.com>
2023-02-12 14:12:36 -0500

    Add changes in creations

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,21 +3,7 @@ import React from 'react'
 const Main = () => {
   return (
     <article className='main'>
-      <div className='main__left'>
-       
-      </div>
-      <div className='main__right'>
-        <div>
-        <small>THE LEADER IN <br />
-        INTERACTIVE VR</small>
-        <p>Founded in 2011, Loopstudios has been producing world-class <br />
-        virtual reality projects for some of the best companies around the <br />
-        globe. Our award-winning creations have transformed <br />
-        businesses through digital experiences that bind to their brand.</p>
 
-        </div>
-        
-      </div>
     </article>
   )
 }

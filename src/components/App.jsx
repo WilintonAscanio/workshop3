81a3a1d8379c57393b73b6a229390bef6a257abf
WilintonAscanio <wilintonascanio12@hotmail.com>
2023-02-12 23:41:55 -0500

    Add design responsive

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Creations from './Creations'
 import Header from './Header'
-import headerMobile from './headerMobile'
 import Main from './Main'
 import Footer from './Footer'
 
@@ -27,7 +26,6 @@ const App = () => {
   return (
     <>
     <Header list = {menu}/>
-    <headerMobile/>
     <Main/>
     <Creations/>
     <Footer list = {menu}/>
--- src/components/Creations.jsx
+++ src/components/Creations.jsx
@@ -15,111 +15,112 @@ import gridMobile from '../assets/images/mobile/image-grid.jpg';
 import soccerMobile from '../assets/images/mobile/image-soccer-team.jpg';
 import nightMobile from '../assets/images/mobile/image-night-arcade.jpg';
 import pocketMobile from '../assets/images/mobile/image-pocket-borealis.jpg';
+
 const Creations = () => {
   const [width, setWidth] = React.useState(window.innerWidth);
   const minWidth = 819;
   const handleChangeWidth = () => setWidth(window.innerWidth);
   window.addEventListener("resize", handleChangeWidth);
-    
+
   const creations = [
     {
       image: deep,
       title: 'DEEP EARTH',
-      imageMobile : deepMobile
+      imageMobile: deepMobile
     },
     {
       image: night,
-      title:'NIGHT ARCADE',
-      imageMobile : nightMobile
+      title: 'NIGHT ARCADE',
+      imageMobile: nightMobile
 
     },
     {
       image: soccer,
       title: 'SOCCER TEAM VR',
-      imageMobile : soccerMobile
+      imageMobile: soccerMobile
 
     },
     {
       image: grid,
       title: 'THE GRID',
-      imageMobile : gridMobile
+      imageMobile: gridMobile
 
     },
     {
       image: fromAbove,
       title: 'FROM UP ABOVE VR',
-      imageMobile : fromAboveMobile
+      imageMobile: fromAboveMobile
 
     },
     {
       image: pocket,
       title: 'POCKET BOREALIS',
-      imageMobile : pocketMobile
+      imageMobile: pocketMobile
 
     },
     {
       image: curiosity,
       title: 'THE CURIOSITY',
-      imageMobile : curiosityMobile
+      imageMobile: curiosityMobile
 
     },
     {
       image: fisheye,
       title: 'MAKE IT FISHEYE',
-      imageMobile : fisheyeMobile
+      imageMobile: fisheyeMobile
 
     }
-  ]  
+  ]
 
   if (width > minWidth) {
     return (
       <article className='creations'>
-          <div className='creations__title'>
-              <small>OUR CREATIONS</small>
-              <button className='creations__button'>SEE ALL</button>
-          </div>
-          <section className='creations__cards'>
-              {creations.map((element,index)=> (
-                 <div key={index} style={{ 
-                  backgroundImage: `url("${element.image}")`
-                }}>
-                  <small>{element.title}</small>
-                </div>
-                
-              ))}
-          
-              
-          </section>
+        <div className='creations__title'>
+          <small>OUR CREATIONS</small>
+          <button className='creations__button'>SEE ALL</button>
+        </div>
+        <section className='creations__cards'>
+          {creations.map((element, index) => (
+            <div key={index} style={{
+              backgroundImage: `url("${element.image}")`
+            }}>
+              <small>{element.title}</small>
+            </div>
+
+          ))}
+
+
+        </section>
       </article>
     )
-    
+
   }
   else {
     return (
       <article className='creations'>
-          <div className='creations__title'>
-              <small>OUR CREATIONS</small>
-              
-          </div>
-          <section className='creations__cards'>
-              {creations.map((element,index)=> (
-                 <div key={index} style={{ 
-                  backgroundImage: `url("${element.imageMobile}")`
-                }}>
-                  <small>{element.title}</small>
-                </div>
-                
-              ))}
-          
-              
-          </section>
-          <button className='creations__button'>SEE ALL</button>
+        <div className='creations__title'>
+          <small>OUR CREATIONS</small>
+
+        </div>
+        <section className='creations__cards'>
+          {creations.map((element, index) => (
+            <div key={index} style={{
+              backgroundImage: `url("${element.imageMobile}")`
+            }}>
+              <small>{element.title}</small>
+            </div>
+
+          ))}
+
+
+        </section>
+        <button className='creations__button'>SEE ALL</button>
       </article>
     )
 
   }
 
-  
+
 }
 
 export default Creations
